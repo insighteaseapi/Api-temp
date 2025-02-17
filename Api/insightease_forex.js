f443c9ae3d111d3aa1d47c30ce5d530cdeb5138f
Insight Ease <insighteaseoffical@gmail.com>
2025-02-17 10:07:09 +0500

    Add files via upload

---
+++ Api/insightease_forex.js
@@ -0,0 +1,303 @@
+class insighteaseForex {
+    constructor(api_key = '') {
+        this.api_key = api_key ? api_key : "api_key";
+        this.output = '';  // default is json
+        this.output_type = 'JSON';  // Default output type JSON
+        this.basic_url = "https://api.insightease.com/forex";
+        this.api_message = "API Key is empty, please set your API Key.";
+    }
+
+    returnError(msg) {
+        return { status: false, msg: msg, Error: "Code or input data error" };
+    }
+
+    checkApiKey() {
+        return this.api_key !== null && this.api_key !== 'api_key';
+    }
+
+    checkSymbolId(txt) {
+        const cleanedTxt = txt.replace(/,/g, "");
+        return /^\d+$/.test(cleanedTxt) ? 'id' : 'symbol';
+    }
+
+    async response(url, params) {
+        if (!this.checkApiKey()) {
+            return this.returnError(this.api_message);
+        }
+
+        if (this.api_key) {
+            params['api_key'] = this.api_key;
+        }
+        if (this.output) {
+            params['output'] = this.output;
+        }
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams(params)
+            });
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+            return this.returnError(error.message);
+        }
+    }
+
+    /**
+     * use to get all supporting symbols
+     * @param {*} type set to forex or crypto
+     * @returns
+     */
+    async getSymbolsList(type = 'forex') {
+        if (type !== 'forex' && type !== 'crypto') {
+            type = 'forex';
+        }
+        const params = { type: type };
+        const link = `${this.basic_url}/list`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get symbol profile details
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @returns
+     */
+    async getProfile(symbol) {
+        symbol = Array.isArray(symbol) ? symbol.join(',') : symbol;
+        if (!symbol) {
+            return this.returnError("Symbol or Id not defined");
+        }
+        const symbolId = this.checkSymbolId(symbol);
+        const params = { [symbolId]: symbol };
+        const link = `${this.basic_url}/profile`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get convertion of currency
+     * @param {*} amount set amunt of currency
+     * @param {*} pairOne set currency symbol name e.g: EUR
+     * @param {*} pairTwo set currency symbol name e.g: USD
+     * @returns
+     */
+    async getConverter(amount = '200', pairOne = '', pairTwo = '') {
+        if (!pairOne) {
+            return this.returnError("Symbol not defined");
+        }
+        const params = { amount: amount };
+        if (pairTwo) {
+            params['pair1'] = pairOne;
+            params['pair2'] = pairTwo;
+        } else {
+            params["symbol"] = pairOne;
+        }
+        const link = `${this.basic_url}/converter`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * Use to get latest price of currency
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @returns
+     */
+    async getLatestPrice(symbol) {
+        if (!symbol) {
+            symbol = Array.isArray(symbol) ? symbol.join(',') : symbol;
+            return this.returnError("Symbol or Id not defined");
+        }
+        const symbolId = this.checkSymbolId(symbol);
+        const params = { [symbolId]: symbol };
+        const link = `${Rthis.basic_url}/latest`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get Base Price
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} type   set to forex or crypto
+     * @param {*} time   set to true or false
+     * @returns
+     */
+    async getBasePrices(symbol, type = "forex", time = false) {
+        symbol = Array.isArray(symbol) ? symbol.join(',') : symbol;
+        if (!symbol) {
+            return this.returnError("Symbol or Id not defined");
+        }
+        const params = { symbol: symbol, type: type };
+        if (time) {
+            params['time'] = 1;
+        }
+        const link = `${this.basic_url}/base_latest`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get last candle
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} period 1m, 5m, 15m, 30m,1h, 2h, 4h, 5h, 1d, 1w, month (all except 1m and 2h)
+     * @param {*} candle set to active, close and both, default: both
+     * @returns
+     */
+    async getLastCandle(symbol, period = '1h', candle = 'both') {
+        symbol = Array.isArray(symbol) ? symbol.join(',') : symbol;
+        // Check if symbol is defined
+        if (!symbol) {
+            return this.returnError("Symbol or Id not defined");
+        }
+        const symbolId = this.checkSymbolId(symbol);
+        // Set up the parameters for the API call
+        const params = { [symbolId]: symbol, period: period, candle: candle };
+        const link = `${this.basic_url}/candle`;
+        return await this.response(link, params);
+    }
+
+
+    /**
+     * use to get history of currency
+     * @param {*} data set all params data
+     * @returns
+     */
+    async getHistory(data) {
+        const id = data.id || '';
+        const symbol = data.symbol || '';
+        const period = data.period || '1h';
+        const level = (data.level > 3) ? 1 : Math.min(Math.max(data.level || 1, 1), 3);
+        const fromDate = data.from || '';
+        const toDate = data.to || '';
+
+        if (!symbol && !id) {
+            return this.returnError("Symbol or Id not defined");
+        }
+
+        const params = {
+            period: period,
+            limit: level
+        };
+        if (id) {
+            params['id'] = id;
+        }
+        if (symbol) {
+            params['symbol'] = symbol;
+        }
+        if (fromDate && toDate) {
+            params['from'] = fromDate;
+            params['to'] = toDate;
+        }
+
+        const link = `${this.basic_url}/history`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * use to get signal
+     * @param {*} symbol pass currency name or id
+     * @param {*} period pass period
+     * @param {*} url    pass to retrieve the signal
+     * @returns
+     */
+    async getSignal(symbol, period, url) {
+        if (!symbol) {
+            return this.returnError("Symbol or Id not defined");
+        }
+        const result = !Array.isArray(symbol) && !symbol.includes(',');
+        if (!result) {
+            return this.returnError("Symbol only accept single id");
+        }
+
+        const symbolId = this.checkSymbolId(symbol);
+        const params = { [symbolId]: symbol, period: period };
+        const link = `${this.basic_url}/${url}`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get pivot points
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} period set period (1m, 5m, 15m, 30m,1h, 2h, 4h, 5h, 1d, 1w, month)
+     * @returns
+     */
+    async getPivotPoints(symbol, period = '1h') {
+        return await this.getSignal(symbol, period, "pivot_points");
+    }
+
+    /**
+     * get moving averages
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} period set period (1m, 5m, 15m, 30m,1h, 2h, 4h, 5h, 1d, 1w, month)
+     * @returns
+     * */
+    async getMovingAverages(symbol, period = '1h') {
+        return await this.getSignal(symbol, period, "ma_avg");
+    }
+
+    /**
+     * get technical indicators
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} period set period (1m, 5m, 15m, 30m,1h, 2h, 4h, 5h, 1d, 1w, month)
+     * @returns
+     */
+    async getTechnicalIndicator(symbol, period = '1h') {
+        return await this.getSignal(symbol, period, "indicators");
+    }
+
+    /**
+     * get economic calendar
+     * @param {*} symbol set currency symbol name e.g: EUR/USD
+     * @param {*} country set country name e.g: US,JP,GB
+     * @param {*} fromDate set from date e.g: 2024-11-13
+     * @param {*} toDate set to date e.g: 2024-11-14
+     * @param {*} event set event name To get all history of event
+     * @returns
+     */
+    async getEconomyCalendar(symbol = '', country = '', fromDate = '', toDate = '', event = '') {
+        if (!this.checkApiKey()) {
+            return this.returnError(this.api_message);
+        }
+
+        const params = { access_key: this.api_key };
+        if (fromDate) {
+            params['from'] = fromDate;
+        }
+        if (toDate) {
+            params['to'] = toDate;
+        }
+        if (symbol) {
+            params['symbol'] = symbol;
+        }
+        if (country) {
+            params['country'] = country;
+        }
+        if (event) {
+            params['event'] = event;
+        }
+
+        if (!symbol && !country && !event) {
+            return this.returnError("At least a symbol, country, or event must be defined");
+        }
+
+        const link = `${this.basic_url}/economy_cal`;
+        return await this.response(link, params);
+    }
+
+    /**
+     * get search query
+     * @param {*} search set search value e.g: EURO Dollar
+     * @param {*} strict set strict value to 0 AND 1 {0: search if any word exist, 1: search if all words exist}
+     * @returns
+     * */
+    async getSearchQuery(search, strict = 0) {
+        if (!search) {
+            return this.returnError("Search value is empty");
+        }
+
+        const params = { s: search, strict: strict };
+        const link = `${this.basic_url}/search`;
+        return await this.response(link, params);
+    }
+}
+
+module.exports = insighteaseForex;
---
+++ Api/main.js
@@ -0,0 +1,205 @@
+const insighteaseForex = require('./insightease_forex.js');
+
+async function main() {
+    // Instantiate insighteaseForex with your API key
+    const forexApi = new insighteaseForex('TrN3njXhvR5Jkqz0VPZIOdHRrE');
+
+    try {
+         const latestPrice = await forexApi.getLatestPrice('GBP/USD');
+         console.log("Latest Price :", latestPrice);
+    } catch (error) {
+        console.error("Error fetching latest prices:", error);
+    }
+
+// Get symbols list
+
+    try {
+        const symbolsList = await forexApi.getSymbolsList();
+        console.log("Symbols List:", symbolsList);
+    } catch (error) {
+        console.error("Error fetching symbols list:", error);
+    }
+
+// Get profile of a currency
+
+    try {
+        const symbolProfile = await forexApi.getProfile("GBP");
+        console.log("Currency Profile :", symbolProfile);
+
+        const symbolProfileMulti = await forexApi.getProfile("EUR,USD,JPY");
+        console.log("Currency Profile:", symbolProfileMulti);
+
+        const symbolProfileId = await forexApi.getProfile("1,2,3,4");
+        console.log("Currency Profile:", symbolProfileId);
+    } catch (error) {
+        console.error("Error fetching currency profiles:", error);
+    }
+
+
+// Convert currency
+
+    try {
+        const conversionResult = await forexApi.getConverter(200, 'EUR', 'USD');
+        console.log("Conversion Result (200 EUR to USD):", conversionResult);
+
+        const conversionResult1 = await forexApi.getConverter(200, 'EUR/USD');
+        console.log("Conversion Result (200 EUR/USD):", conversionResult1);
+    } catch (error) {
+        console.error("Error converting currency:", error);
+    }
+
+// Get Latest Price
+
+    try {
+        const latestPriceAllForex = await forexApi.getLatestPrice("all_forex");
+        console.log("Latest Price for all_forex:", latestPriceAllForex);
+
+        const latestPrice = await forexApi.getLatestPrice('GBP/USD');
+        console.log("Latest Price :", latestPrice);
+
+        const latestPriceMultiple = await forexApi.getLatestPrice(["EUR/USD", "JPY/USD"]);
+        console.log("Latest Price for multiple currencies:", latestPriceMultiple);
+
+        const latestPriceById = await forexApi.getLatestPrice("40");
+        console.log("Latest Price by ID:", latestPriceById);
+    } catch (error) {
+        console.error("Error fetching latest prices:", error);
+    }
+
+
+// Get Base Prices
+
+    try {
+        const basePricesEUR = await forexApi.getBasePrices("EUR");
+        console.log("Base Prices for EUR:", basePricesEUR);
+
+        const basePricesForex = await forexApi.getBasePrices('EUR', 'forex', true);
+        console.log("Base Prices for EUR with forex:", basePricesForex);
+
+        const basePricesCrypto = await forexApi.getBasePrices('EUR', 'crypto');
+        console.log("Base Prices for EUR and crypto:", basePricesCrypto);
+    } catch (error) {
+        console.error("Error fetching base prices:", error);
+    }
+
+
+// Get Last Candle
+    
+    try {
+        const lastCandle5m = await forexApi.getLastCandle("all_forex", '5m');
+        console.log("Last Candle for 5m:", lastCandle5m);
+
+        const lastCandle1h = await forexApi.getLastCandle("1", '1h', 'both');
+        console.log("Last Candle for 1h:", lastCandle1h);
+
+        const lastCandle1d = await forexApi.getLastCandle("all_forex", '1d');
+        console.log("Last Candle for 1d:", lastCandle1d);
+
+        const candleData = await forexApi.getLastCandle("1,2,3", '1h');
+        console.log("Candle Data for IDs:", candleData);
+    } catch (error) {
+        console.error("Error fetching candle data:", error);
+    }
+
+    
+// Get Currency History
+    
+    try {
+        const historyData1 = await forexApi.getHistory({
+            id: '98',
+            period: '1d',
+            level: '10',
+            from: '2024-01-01',
+            to: '2024-10-31'
+        });
+        console.log("History Data for different currencies:", historyData1);
+
+        const historyData2 = await forexApi.getHistory({
+            id: '1',
+            period: '1h'
+        });
+        console.log("History Data for EURUSD:", historyData2);
+    } catch (error) {
+        console.error("Error fetching currency history:", error);
+    }
+
+    
+// Get Pivot Points
+    
+
+    async function getPivotPoints() {
+        let pivotPoints = await forexApi.getPivotPoints('2', '1h');
+        console.log('Pivot Points:', pivotPoints);
+        console.log('Pivot Points1:', pivotPoints.response.pivot_point);
+        console.log('Pivot Points2:', pivotPoints.response.pivot_point.classic);
+    }
+    getPivotPoints();
+
+    try {
+
+        const pivotPointsEURUSD = await forexApi.getPivotPoints("EUR/USD", '1d');
+        console.log("Pivot Points for EUR/USD:", pivotPointsEURUSD);
+
+        const pivotPointsUSD = await forexApi.getPivotPoints('9', '1d');
+        console.log("Pivot Points for USD:", pivotPointsUSD.response.pivot_point);
+
+    } catch (error) {
+        console.error("Error fetching pivot points:", error);
+    }
+
+    
+// Get Moving Averages
+    
+    try {
+        const movingAverages1 = await forexApi.getMovingAverages("1", '1d');
+        console.log("Moving Averages for 1:", movingAverages1);
+
+        const movingAveragesEURUSD = await forexApi.getMovingAverages('NZD/USD', '1h');
+        console.log("Moving Averages for EUR/USD:", movingAveragesEURUSD);
+    } catch (error) {
+        console.error("Error fetching moving averages:", error);
+    }
+
+    
+// Get Technical Indicators
+ 
+    try {
+        const indicators1 = await forexApi.getTechnicalIndicator("1", '1d');
+        console.log("Technical Indicators for 1:", indicators1);
+
+        const indicatorsEURUSD = await forexApi.getTechnicalIndicator("EUR/USD", '1d');
+        console.log("Technical Indicators for EUR/USD:", indicatorsEURUSD);
+    } catch (error) {
+        console.error("Error fetching technical indicators:", error);
+    }
+
+    
+// Get Economy Calendar
+    
+    try {
+        const economyCalendarUSD = await forexApi.getEconomyCalendar("USD", '2024-01-01', '2024-10-31');
+        console.log("Economy Calendar for USD:", economyCalendarUSD);
+
+        const economyCalendarUSDJPY = await forexApi.getEconomyCalendar("USD,JPY");
+        console.log("Economy Calendar for USD and JPY:", economyCalendarUSDJPY);
+    } catch (error) {
+        console.error("Error fetching economy calendar:", error);
+    }
+
+    
+// Search API
+    
+    try {
+        const searchResultsStrict0 = await forexApi.getSearchQuery("BTC Dollar", 0);
+        console.log("Search Results (strict=0):", searchResultsStrict0);
+
+        const searchResultsStrict1 = await forexApi.getSearchQuery("BTC Dollar", 1);
+        console.log("Search Results (strict=1):", searchResultsStrict1);
+    } catch (error) {
+        console.error("Error fetching search results:", error);
+    }
+
+
+}
+
+main().catch(error => console.error("Error in main function:", error));
